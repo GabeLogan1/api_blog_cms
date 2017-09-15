19ff2414f8e26d991334909769745e122d01b177
Basit Butt <basitadnan21@hotmail.com>
2017-09-16 00:02:33 +0500

    basic auth authentication added

--- app/controllers/api_blog_cms/welcome_controller.rb
+++ app/controllers/api_blog_cms/welcome_controller.rb
@@ -1,10 +1,23 @@
 require_dependency "api_blog_cms/application_controller"
-
 module ApiBlogCms
   class WelcomeController < ApplicationController
+    before_action :authenticate
+
+
     def index
+      
       blog=Blog.all
       render json: blog, each_serializer: BlogSerializer
     end
+
+
+    protected
+        def authenticate
+  
+          authenticate_or_request_with_http_basic do|email, password|
+          @user = CamaleonCms::User.by_email(email).first
+          @check = @user && @user.authenticate(password)
+          end
+        end
   end
 end
