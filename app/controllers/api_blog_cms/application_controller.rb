81156b7d11ad795fa00e6dab6731e2f092efbbfd
Basit Butt <basitadnan21@hotmail.com>
2017-09-16 16:11:11 +0500

    Added authentication to application controller

--- app/controllers/api_blog_cms/application_controller.rb
+++ app/controllers/api_blog_cms/application_controller.rb
@@ -1,5 +1,16 @@
 module ApiBlogCms
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :authenticate
+
+    protected
+    def authenticate
+      authenticate_or_request_with_http_basic do|email, password|
+      @user = CamaleonCms::User.by_email(email).first
+      @user && @user.authenticate(password)
+      end
+    end
+
+
     end
 end
