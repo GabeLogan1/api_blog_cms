3673cc1f9e175f0e1a2163b9fd3556ba8c828035
Basit Butt <basitadnan21@hotmail.com>
2017-09-21 00:56:39 +0500

    qury parameter functional for category and sanitizer added for posts

--- app/serializers/api_blog_cms/blog_serializer.rb
+++ app/serializers/api_blog_cms/blog_serializer.rb
@@ -1,5 +1,10 @@
 module ApiBlogCms
   class BlogSerializer < ActiveModel::Serializer
     attributes :id, :content
+
+    def content
+      full_sanitizer = Rails::Html::FullSanitizer.new
+      full_sanitizer.sanitize(object.content)
+    end
   end
 end
