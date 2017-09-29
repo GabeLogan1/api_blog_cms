a1781c53834c7ba740ac828e53bf5455c77d9602
Basit Butt <basitadnan21@hotmail.com>
2017-09-29 20:35:54 +0500

    quikfix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ApiBlogCms::Engine.routes.draw do
   get 'category/index'
   get 'blog/index'
   
-  get 'blog' =>'blog#index'
-  get 'category' =>'category#index'
+  get 'blogs' =>'blog#index'
+  get 'categories' =>'category#index'
 	#root 'welcome#index'
 end
